aec82566ad3ebc98e826805ef777b1b3f54108c2
ashrafomid2025 <omidmashraf@gmail.com>
2026-02-06 13:05:07 +0430

    tables strtures added

---
+++ app/Models/Author.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    /** @use HasFactory<\Database\Factories\AuthorFactory> */
+    use HasFactory;
+    protected $fillable = [
+        "name",
+        "bio",
+        "nationality"
+    ];
+    public function book(){
+        return $this->hasMany(Book::class,'author_id');
+    }
+}
