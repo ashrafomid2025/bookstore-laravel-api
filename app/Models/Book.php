2fcca655ff983a94b8fb0ff8a0e421f2c67d716b
ashrafomid2025 <omidmashraf@gmail.com>
2026-02-07 14:17:12 +0430

    api controller for author added

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,7 +9,36 @@ class Book extends Model
 {
     /** @use HasFactory<\Database\Factories\BookFactory> */
     use HasFactory;
+    protected $fillable = [
+        "title",
+        "isbn",
+        "description",
+        "published_at",
+        "total_copies",
+        "available_copies",
+        "genre",
+        "price",
+        "cover_image",
+        "status",
+        "author_id"
+    ];
     public function author(){
         return $this->belongsTo(Author::class, 'author_id');
+    }
+    public function borrowing(){
+        return $this->hasMany(Borrowing::class);
+    }
+    public function isAvailable(){
+        return $this->available_copies> 0;
+    }
+    public function borrow(){
+        if($this->available_copies> 0){
+            $this->decrement('available_copies');
+        }
+    }
+    public function returnBook(){
+        if($this->avaiable_copies< $this->total_copies){
+            $this->increment('avaiable_copies');
+        }
     }
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -9,4 +9,22 @@ class Member extends Model
 {
     /** @use HasFactory<\Database\Factories\MemberFactory> */
     use HasFactory;
+    protected $fillable = [
+        "name",
+        "email",
+        "whatsApp_number",
+        "address",
+        "membership_date",
+        "status",
+
+    ];
+    protected $casts = [
+        "membership_date"=>"date"
+    ];
+    public function borrowing(){
+        return $this->belongsTo(Borrowing::class);
+    }
+    public function activeBorrowing(){
+        return $this->borrowing()->where('status','borrowed');
+    }
 }
